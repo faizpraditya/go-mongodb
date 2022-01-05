c06fdc1751d800122f392daeeab949c8c8b6c539
pradityamf <pradityamf@gmail.com>
2022-01-05 13:37:15 +0700

    Simple implement go mongodb

---
+++ student.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+// kalau ga dikasi struct tag, bakal jadi lowercase
+type Student struct {
+	Id       primitive.ObjectID `bson:"_id"`
+	Name     string
+	Gender   string
+	Age      int
+	JoinDate time.Time `bson:"joinDate"`
+	IdCard   string    `bson:"idCard"`
+	Senior   bool
+}
